dd03a331394493afa2a31da7aec96884c868317a
Veron3373 <veron3373v@gmail.com>
2022-10-17 00:14:32 +0300

    +

--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -1,121 +1,65 @@
 export const galleryItems = [
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279835521_3230430207189268_7094619722763031627_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279835521_3230430207189268_7094619722763031627_n.jpg',
-    description: 'Запоріжжя',
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279206716_307057581584810_4080382932086249522_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279206716_307057581584810_4080382932086249522_n.jpg',
-  description: 'Херсон',
+      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276964023_481331730381529_1960830792613264482_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276964023_481331730381529_1960830792613264482_n.jpg',
-    description: 'Миколаїв',
+      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276965201_3101181910196954_7870409388626179933_n.jpg',
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276965201_3101181910196954_7870409388626179933_n.jpg',
-    description: 'Харків',
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277030249_5239061839448176_7265763834340444006_n.jpg',
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277030249_5239061839448176_7265763834340444006_n.jpg',
-    description: 'Конотоп',
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279233848_165812285810927_2489052314037515981_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279233848_165812285810927_2489052314037515981_n.jpg',
-    description: 'Чорнобиль',
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277771211_3514008425527920_5631564176589336968_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277771211_3514008425527920_5631564176589336968_n.jpg',
-    description: 'Ірпінь',
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281152268_790150808614974_2817729401704735721_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281152268_790150808614974_2817729401704735721_n.jpg',
-    description: 'Гостомель',
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/286617009_581983086689971_7294171290639466325_n.jpg',
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/286617009_581983086689971_7294171290639466325_n.jpg',
-    description: 'Северодонецьк',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/292193204_1197605507751986_2744072691933261989_n-800x948.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/292193204_1197605507751986_2744072691933261989_n-800x948.jpg',
-    description: 'Лисичансько',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276234357_709576230054217_4031173660700798207_n-800x1000.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276234357_709576230054217_4031173660700798207_n-800x1000.jpg',
-    description: 'Маріупіль',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277321882_2171816979640376_3440499094048076903_n.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277321882_2171816979640376_3440499094048076903_n.jpg',
-    description: 'Чорнобаївка',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/287937103_435200761463554_2819871043830514937_n.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/287937103_435200761463554_2819871043830514937_n.jpg',
-    description: 'Дніпро',
-  },
-  {
-    preview:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278882627_530732232012795_7204989664818435432_n.jpg',
-    original:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278882627_530732232012795_7204989664818435432_n.jpg',
-    description: 'Одеса',
-  },
-  {
-    preview:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278506010_556796668986274_941904973172398525_n.jpg',
-    original:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278506010_556796668986274_941904973172398525_n.jpg',
-    description: 'Суми',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281040292_535018524940379_2554795777280686572_n.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281040292_535018524940379_2554795777280686572_n.jpg',
-    description: 'Львів',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277822006_158772786592617_1415336759958899975_n.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277822006_158772786592617_1415336759958899975_n.jpg',
-    description: 'Буча',
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
   },
 ];
