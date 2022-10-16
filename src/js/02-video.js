e10ff6078dff0fb87c92032e9c3b79138547782a
Veron3373 <veron3373v@gmail.com>
2022-10-16 23:03:40 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,14 +3,33 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-const lastViewTime = 'videoplayer-current-time'
 
+const STORAGE_KEY = 'videoplayer-current-time';
 
-function onPlay({ seconds }) {
-  localStorage.setItem(lastViewTime, seconds);
+const onPlay = function (data) {
+  localStorage.setItem(STORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+
+if (savedData) {
+  player.setCurrentTime(localStorage.getItem(STORAGE_KEY));
 }
 
-player.on('timeupdate', throttle(onPlay, 500));
 
-player.setCurrentTime(localStorage.getItem(lastViewTime));
+
+
+
+
+//!Коротший але видає помилку (працює)
+
+//player.on('timeupdate', throttle(onPlay, 500));
+
+//function onPlay({ seconds }) {
+//  localStorage.setItem(lastViewTime, seconds);
+//}
+
+//player.setCurrentTime(localStorage.getItem(lastViewTime));
 
