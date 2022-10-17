bd980adf9186861dc8deb35278a17e158d285cac
Veron3373 <veron3373v@gmail.com>
2022-10-17 09:47:57 +0300

    +

--- src/js/gallery-items.js
+++ src/js/gallery-items.js
@@ -1,184 +1,184 @@
 export const galleryItems = [
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+  //  description: 'Hokkaido Flower',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+  //  description: 'Container Haulage Freight',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+  //  description: 'Aerial Beach View',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+  //  description: 'Flower Blooms',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+  //  description: 'Alpine Mountains',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+  //  description: 'Mountain Lake Sailing',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+  //  description: 'Alpine Spring Meadows',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+  //  description: 'Nature Landscape',
+  //},
+  //{
+  //  preview:
+  //    'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+  //  original:
+  //    'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+  //  description: 'Lighthouse Coast Sea',
+  //},
   {
     preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      'https://habrastorage.org/webt/do/dy/ag/dodyaggccdf5nq5jn6ughu9fs_o.jpeg',
     original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-  {
-    preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279835521_3230430207189268_7094619722763031627_n.jpg',
-    original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279835521_3230430207189268_7094619722763031627_n.jpg',
+      'https://habrastorage.org/webt/do/dy/ag/dodyaggccdf5nq5jn6ughu9fs_o.jpeg',
     description: 'Запоріжжя',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279206716_307057581584810_4080382932086249522_n.jpg',
+      'https://habrastorage.org/webt/fr/4m/6k/fr4m6k4jyrub_kfgj_1kmsuu66e.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279206716_307057581584810_4080382932086249522_n.jpg',
+      'https://habrastorage.org/webt/fr/4m/6k/fr4m6k4jyrub_kfgj_1kmsuu66e.jpeg',
     description: 'Херсон',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276964023_481331730381529_1960830792613264482_n.jpg',
+      'https://habrastorage.org/webt/ue/gb/v5/uegbv59opz8ffljcydc_tp1ojum.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276964023_481331730381529_1960830792613264482_n.jpg',
+      'https://habrastorage.org/webt/ue/gb/v5/uegbv59opz8ffljcydc_tp1ojum.jpeg',
     description: 'Миколаїв',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276965201_3101181910196954_7870409388626179933_n.jpg',
+      'https://habrastorage.org/webt/yv/rh/fd/yvrhfd5pcz66h-bulcfbt9j1mue.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276965201_3101181910196954_7870409388626179933_n.jpg',
+      'https://habrastorage.org/webt/yv/rh/fd/yvrhfd5pcz66h-bulcfbt9j1mue.jpeg',
     description: 'Харків',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277030249_5239061839448176_7265763834340444006_n.jpg',
+      'https://habrastorage.org/webt/v5/kp/rr/v5kprryakf1s23bfuf1tzgzkc1m.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277030249_5239061839448176_7265763834340444006_n.jpg',
+      'https://habrastorage.org/webt/v5/kp/rr/v5kprryakf1s23bfuf1tzgzkc1m.jpeg',
     description: 'Конотоп',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279233848_165812285810927_2489052314037515981_n.jpg',
+      'https://habrastorage.org/webt/4r/xo/5e/4rxo5ebkclmwxrdkjdc3wuqffzs.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/279233848_165812285810927_2489052314037515981_n.jpg',
+      'https://habrastorage.org/webt/4r/xo/5e/4rxo5ebkclmwxrdkjdc3wuqffzs.jpeg',
     description: 'Чорнобиль',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277771211_3514008425527920_5631564176589336968_n.jpg',
+      'https://habrastorage.org/webt/6j/uz/5c/6juz5c7xvbt2iokkg1k5_bapvp8.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277771211_3514008425527920_5631564176589336968_n.jpg',
+      'https://habrastorage.org/webt/6j/uz/5c/6juz5c7xvbt2iokkg1k5_bapvp8.jpeg',
     description: 'Ірпінь',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281152268_790150808614974_2817729401704735721_n.jpg',
+      'https://habrastorage.org/webt/of/ge/ln/ofgelngm8vujgbts67bermhdsxo.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281152268_790150808614974_2817729401704735721_n.jpg',
+      'https://habrastorage.org/webt/of/ge/ln/ofgelngm8vujgbts67bermhdsxo.jpeg',
     description: 'Гостомель',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/286617009_581983086689971_7294171290639466325_n.jpg',
+      'https://habrastorage.org/webt/kw/6f/d1/kw6fd1fwr6uug77lldzkqgfdpng.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/286617009_581983086689971_7294171290639466325_n.jpg',
+      'https://habrastorage.org/webt/kw/6f/d1/kw6fd1fwr6uug77lldzkqgfdpng.jpeg',
     description: 'Северодонецьк',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/292193204_1197605507751986_2744072691933261989_n-800x948.jpg',
+      'https://habrastorage.org/webt/ke/ax/bi/keaxbibvns5ubiegqa-nbpyokjg.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/292193204_1197605507751986_2744072691933261989_n-800x948.jpg',
+      'https://habrastorage.org/webt/ke/ax/bi/keaxbibvns5ubiegqa-nbpyokjg.jpeg',
     description: 'Лисичансько',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276234357_709576230054217_4031173660700798207_n-800x1000.jpg',
+      'https://habrastorage.org/webt/-b/xq/q6/-bxqq66lfb5z3rygmgigjknu4w4.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/276234357_709576230054217_4031173660700798207_n-800x1000.jpg',
+      'https://habrastorage.org/webt/-b/xq/q6/-bxqq66lfb5z3rygmgigjknu4w4.jpeg',
     description: 'Маріупіль',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277321882_2171816979640376_3440499094048076903_n.jpg',
+      'https://habrastorage.org/webt/vs/hw/8t/vshw8tkb2ku1re4x4ld83g6wxrw.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277321882_2171816979640376_3440499094048076903_n.jpg',
+      'https://habrastorage.org/webt/vs/hw/8t/vshw8tkb2ku1re4x4ld83g6wxrw.jpeg',
     description: 'Чорнобаївка',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/287937103_435200761463554_2819871043830514937_n.jpg',
+      'https://habrastorage.org/webt/th/hp/ip/thhpipolhtef1wsoxsks89lzyco.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/287937103_435200761463554_2819871043830514937_n.jpg',
+      'https://habrastorage.org/webt/th/hp/ip/thhpipolhtef1wsoxsks89lzyco.jpeg',
     description: 'Дніпро',
   },
   {
     preview:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278882627_530732232012795_7204989664818435432_n.jpg',
+      'https://habrastorage.org/webt/ns/qs/ve/nsqsvefmrxacmwiaoxei4moiuv0.jpeg',
     original:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278882627_530732232012795_7204989664818435432_n.jpg',
+      'https://habrastorage.org/webt/ns/qs/ve/nsqsvefmrxacmwiaoxei4moiuv0.jpeg',
     description: 'Одеса',
   },
   {
     preview:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278506010_556796668986274_941904973172398525_n.jpg',
+      'https://habrastorage.org/webt/fk/hj/fa/fkhjfaqnr4ke3zby7gc9w3f3mok.jpeg',
     original:
-      '	http://iz.com.ua/wp-content/uploads/2022/07/278506010_556796668986274_941904973172398525_n.jpg',
+      'https://habrastorage.org/webt/fk/hj/fa/fkhjfaqnr4ke3zby7gc9w3f3mok.jpeg',
     description: 'Суми',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281040292_535018524940379_2554795777280686572_n.jpg',
+      'https://habrastorage.org/webt/ek/qm/9b/ekqm9bjxraz94vthu-rwyminqh4.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/281040292_535018524940379_2554795777280686572_n.jpg',
+      'https://habrastorage.org/webt/ek/qm/9b/ekqm9bjxraz94vthu-rwyminqh4.jpeg',
     description: 'Львів',
   },
   {
     preview:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277822006_158772786592617_1415336759958899975_n.jpg',
+      'https://habrastorage.org/webt/vp/qo/d3/vpqod3x3bnmmqsxyrkqp6kuz-x0.jpeg',
     original:
-      'http://iz.com.ua/wp-content/uploads/2022/07/277822006_158772786592617_1415336759958899975_n.jpg',
+      'https://habrastorage.org/webt/vp/qo/d3/vpqod3x3bnmmqsxyrkqp6kuz-x0.jpeg',
     description: 'Буча',
   },
 ];
